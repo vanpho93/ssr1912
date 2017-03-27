1e6ea9ae9b09160f6a2122e63074c62f0f77646e
Admin <vanpho02@gmail.com>
2017-03-27 19:49:17 +0700

    reducer

--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ import List from './oldComponent/List';
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const redux = require('redux');
+
+const defaultState = { 
+    mang: [
+        { id: 1, subject: 'Hoc Tap', content: 'Lam bai tap ve nha', important: false},
+        { id: 2, subject: 'Hoc Tap', content: 'Nop do an cuoi khoa', important: true},
+        { id: 3, subject: 'An Choi', content: 'Di choi 8/3', important: false}
+    ] 
+};
+
+const reducer = (state = defaultState, action) => {
+
+};
+
 ReactDOM.render(
     <List />,
     document.getElementById('root')
